48b6cd43c5e27a7118720fd2825c0564966f4478
Dell <Dell@DESKTOP-SO3FE79>
2022-05-28 13:30:56 +0530

    Updated base class and add new method for Opera browser

--- Maven_Amazon/src/test/java/basePackage/BaseClass.java
+++ Maven_Amazon/src/test/java/basePackage/BaseClass.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class BaseClass {
 	
@@ -23,6 +24,12 @@ public class BaseClass {
 	{
 		System.setProperty("webdriver.edge.driver", "C:\\Selenium\\Edge Driver\\msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
+		return driver ;
+	}
+	public static WebDriver openOperaBrowser()
+	{
+		System.setProperty("webdriver.opera.driver", "C:\\Selenium\\Opera Driver\\msedgedriver.exe");
+		WebDriver driver = new OperaDriver();
 		return driver ;
 	}
 
